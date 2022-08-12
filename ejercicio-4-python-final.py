1486f34d1d160e8257aea5586017aa4cf8c39c05
Matías Raúl Ibarra <84516115+MatiasRaulIbarra@users.noreply.github.com>
2022-08-11 22:06:22 -0300

    Update ejercicio-4-python-final.py
    agrego información  sobre el ejercicio 4 de py que realice

--- ejercicio-4-python-final.py
+++ ejercicio-4-python-final.py
@@ -1,4 +1,9 @@
+
+#4)Crear un lista en Python denominado “Perro2” que contenga los siguientes valores: 14,  Fido,  12/12/2012 , Macho, 23546987
+     
+#4)Recorrer la lista “Perro” con un bucle y mostrar sus elementos por pantalla , comenzando desde el último hasta el primero.
+#Lo que hago es crear una lista que contenga los datos del perro
 Perro2 = [14, "Fido", "12/12/2012", "Macho", 23546987]
-#Lo que hago es crear una lista que contenga
+#mediante un for recorro la lista usando la funcion reverse para mostrar los datos de atras hacia adelante
 for i in reversed(Perro2):
     print(i)
