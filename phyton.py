23b71c3690783a8c0b8ea08352586da31af27e02
Macanuda <106766129+Macanuda@users.noreply.github.com>
2022-08-13 15:13:55 -0300

    #Ej nº 7

---
+++ phyton.py
@@ -0,0 +1,5 @@
+Dueno = [28957346,  "Juan",  "Perez", 4789689,  "Belgrano 101"]
+# datos correspondientes con DNI, Nombre, Apellido, Teléfono, Dirección
+# imprimimos el Teléfono si el DNI es mayor a 26000000
+if Dueno[0] > 26000000:
+    print ("Teléfono:", Dueno[3])
